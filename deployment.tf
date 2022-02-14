4627107304e03876895845115690dc0df7f0b6d1
Muniraju N <munirajur2014@gmail.com>
2022-02-14 15:21:18 +0530

    Add deployment trigger for api gateway
    Authors: @KishanAnjanaKharwar @munirajun

--- deployment.tf
+++ deployment.tf
@@ -1,6 +1,14 @@
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = var.api_gateway_id
   stage_name = ""
+  triggers = {
+    redeployment = sha1(jsonencode([
+      var.api_gateway_resource,
+      aws_api_gateway_method.any,
+      aws_api_gateway_integration.lambda_integration
+    ]))
+  }
+
   depends_on = [
     aws_api_gateway_integration.lambda_integration
   ]
